6f2cd4cfad2fd964c2ee00efafb619ca317e64aa
Debika-ViTWO <dbasu@vitwo.in>
2025-04-24 16:22:40 +0530

    updated

---
+++ src/test/java/com/example/dockerdemo/demo/DockerSpringMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dockerdemo.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerSpringMysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
